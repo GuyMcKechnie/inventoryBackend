eba2b684666ad7ddf50d2209fca98e4999c994a9
Guy McKechnie <mckechnieguy.mg@gmail.com>
2025-05-09 12:24:31 +0200

    refactor: Remove error handling during application startup in Main class

--- src/main/java/com/booklidio/booklidio_spring_backend/Inventory/BookController.java
+++ src/main/java/com/booklidio/booklidio_spring_backend/Inventory/BookController.java
@@ -1,12 +1,12 @@
 package com.booklidio.booklidio_spring_backend.Inventory;
 
+import org.springframework.http.ResponseEntity;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +27,6 @@ public class BookController {
     private final BookService bookService;
     private static final Logger logger = LoggerFactory.getLogger(BookController.class);
 
-    @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
@@ -53,16 +52,21 @@ public class BookController {
     }
 
     @PostMapping("/addBook/")
-    public ResponseEntity<Book> addBook(@RequestBody BookDTO bookDTO) {
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
         try {
-            Book book = new Book();
-            book.setIsbn(bookDTO.getIsbn());
-            book.setTitle(bookDTO.getTitle());
-            book.setGrade(bookDTO.getGrade());
-            book.setNewPrice(bookDTO.getNewPrice());
-            book.setUsedPrice(bookDTO.getUsedPrice());
-            book.setCostPrice(bookDTO.getCostPrice());
-            bookService.addBook(book);
+            Book newBook = new Book();
+            Optional<Book> existingBook = bookService.getBook(book.getIsbn());
+            if (existingBook.isPresent()) {
+                logger.error("Book with ISBN: {} already exists", book.getIsbn());
+                return new ResponseEntity<>(HttpStatus.CONFLICT);
+            }
+            newBook.setIsbn(book.getIsbn());
+            newBook.setTitle(book.getTitle());
+            newBook.setGrade(book.getGrade());
+            newBook.setNewPrice(book.getNewPrice());
+            newBook.setUsedPrice(book.getUsedPrice());
+            newBook.setCostPrice(book.getCostPrice());
+            bookService.addBook(newBook);
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
             logger.error("Error adding book", e);
@@ -71,16 +75,16 @@ public class BookController {
     }
 
     @PutMapping("/editBook/{isbn}")
-    public ResponseEntity<Book> editBook(@PathVariable("isbn") String isbn, @RequestBody BookDTO bookDTO) {
+    public ResponseEntity<Book> editBook(@PathVariable("isbn") String isbn, @RequestBody Book book) {
         try {
-            Book book = new Book();
-            book.setIsbn(bookDTO.getIsbn());
-            book.setTitle(bookDTO.getTitle());
-            book.setGrade(bookDTO.getGrade());
-            book.setNewPrice(bookDTO.getNewPrice());
-            book.setUsedPrice(bookDTO.getUsedPrice());
-            book.setCostPrice(bookDTO.getCostPrice());
-            bookService.editBook(isbn, book);
+            Book newBook = new Book();
+            newBook.setIsbn(book.getIsbn());
+            newBook.setTitle(book.getTitle());
+            newBook.setGrade(book.getGrade());
+            newBook.setNewPrice(book.getNewPrice());
+            newBook.setUsedPrice(book.getUsedPrice());
+            newBook.setCostPrice(book.getCostPrice());
+            bookService.editBook(isbn, newBook);
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
             logger.error("Error editing book with ISBN: {}", isbn, e);
@@ -94,18 +98,18 @@ public class BookController {
             bookService.duplicateBook(isbn);
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
-            logger.error("Error duplicating book with ISBN: {}", isbn, e);
+            logger.error("Error duplicating book with ISBN: {}{}", isbn, e.getMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @PostMapping("/deleteBook/{isbn}")
+    @DeleteMapping("/deleteBook/{isbn}")
     public ResponseEntity<Book> deleteBook(@PathVariable("isbn") String isbn) {
         try {
             bookService.deleteBook(isbn);
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
-            logger.error("Error deleting book with ISBN: {}", isbn, e);
+            logger.error("Error deleting book with ISBN: {}{}", isbn, e.getMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
--- src/main/java/com/booklidio/booklidio_spring_backend/Main.java
+++ src/main/java/com/booklidio/booklidio_spring_backend/Main.java
@@ -1,21 +1,12 @@
 package com.booklidio.booklidio_spring_backend;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import ch.qos.logback.classic.Logger;
-
 @SpringBootApplication
 public class Main {
-  private static final Logger logger = (Logger) LoggerFactory.getLogger(Main.class);
 
   public static void main(String[] args) {
-    try {
-      SpringApplication.run(Main.class, args);
-    } catch (Exception e) {
-      logger.error("Error starting Booklidio Backend: ", e);
-      System.exit(1);
-    }
+    SpringApplication.run(Main.class, args);
   }
 }
